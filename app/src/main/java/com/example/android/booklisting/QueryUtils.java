8fa731989c7cc9a65cce89d2a54b19dd3ab51347
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-11-07 14:16:21 +0700

    Modify QueryUtils.java to accept HTTP request

--- app/src/main/java/com/example/android/booklisting/QueryUtils.java
+++ app/src/main/java/com/example/android/booklisting/QueryUtils.java
@@ -1,128 +1,67 @@
 package com.example.android.booklisting;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 public final class QueryUtils {
 
     /** Tag for the log messages */
     private static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
-
-
-    private static final String SAMPLE_JSON_RESPONSE = "{\n" +
-            " \"kind\": \"books#volumes\",\n" +
-            " \"totalItems\": 407,\n" +
-            " \"items\": [\n" +
-            "  {\n" +
-            "   \"kind\": \"books#volume\",\n" +
-            "   \"id\": \"qKFDDAAAQBAJ\",\n" +
-            "   \"etag\": \"tDtdXdwZgPg\",\n" +
-            "   \"selfLink\": \"https://www.googleapis.com/books/v1/volumes/qKFDDAAAQBAJ\",\n" +
-            "   \"volumeInfo\": {\n" +
-            "    \"title\": \"Android\",\n" +
-            "    \"authors\": [\n" +
-            "     \"P.K. Dixit\", \"J.W. Marriot\"\n" +
-            "    ],\n" +
-            "    \"publisher\": \"Vikas Publishing House\",\n" +
-            "    \"publishedDate\": \"2014\",\n" +
-            "    \"description\": \"Android is a movement that has transferred data from laptop to hand-held devices like mobiles. Though there are alternate technologies that compete with Android, but it is the front runner in mobile technology by a long distance. Good knowledge in basic Java will help you to understand and develop Android technology and apps. Many universities in India and across the world are now teaching Android in their syllabus, which shows the importance of this subject. This book can be read by anyone who knows Java and XML concepts. It includes a lot of diagrams along with explanations to facilitate better understanding by students. This book aptly concludes with a project that uses Android, which will greatly benefit students in learning the practical aspects of Android. Key Features • Instructions in designing different Android user interfaces • Thorough explanations of all activities • JSON • Android-based project to aid practical understanding\",\n" +
-            "    \"industryIdentifiers\": [\n" +
-            "     {\n" +
-            "      \"type\": \"ISBN_13\",\n" +
-            "      \"identifier\": \"9789325977884\"\n" +
-            "     },\n" +
-            "     {\n" +
-            "      \"type\": \"ISBN_10\",\n" +
-            "      \"identifier\": \"9325977885\"\n" +
-            "     }\n" +
-            "    ],\n" +
-            "    \"readingModes\": {\n" +
-            "     \"text\": false,\n" +
-            "     \"image\": true\n" +
-            "    },\n" +
-            "    \"pageCount\": 372,\n" +
-            "    \"printType\": \"BOOK\",\n" +
-            "    \"categories\": [\n" +
-            "     \"Computers\"\n" +
-            "    ],\n" +
-            "    \"maturityRating\": \"NOT_MATURE\",\n" +
-            "    \"allowAnonLogging\": false,\n" +
-            "    \"contentVersion\": \"preview-1.0.0\",\n" +
-            "    \"panelizationSummary\": {\n" +
-            "     \"containsEpubBubbles\": false,\n" +
-            "     \"containsImageBubbles\": false\n" +
-            "    },\n" +
-            "    \"imageLinks\": {\n" +
-            "     \"smallThumbnail\": \"http://books.google.com/books/content?id=qKFDDAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api\",\n" +
-            "     \"thumbnail\": \"http://books.google.com/books/content?id=qKFDDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api\"\n" +
-            "    },\n" +
-            "    \"language\": \"en\",\n" +
-            "    \"previewLink\": \"http://books.google.co.id/books?id=qKFDDAAAQBAJ&printsec=frontcover&dq=android&hl=&cd=1&source=gbs_api\",\n" +
-            "    \"infoLink\": \"https://play.google.com/store/books/details?id=qKFDDAAAQBAJ&source=gbs_api\",\n" +
-            "    \"canonicalVolumeLink\": \"https://market.android.com/details?id=book-qKFDDAAAQBAJ\"\n" +
-            "   },\n" +
-            "   \"saleInfo\": {\n" +
-            "    \"country\": \"ID\",\n" +
-            "    \"saleability\": \"FOR_SALE\",\n" +
-            "    \"isEbook\": true,\n" +
-            "    \"listPrice\": {\n" +
-            "     \"amount\": 218750.0,\n" +
-            "     \"currencyCode\": \"IDR\"\n" +
-            "    },\n" +
-            "    \"retailPrice\": {\n" +
-            "     \"amount\": 148750.0,\n" +
-            "     \"currencyCode\": \"IDR\"\n" +
-            "    },\n" +
-            "    \"buyLink\": \"https://play.google.com/store/books/details?id=qKFDDAAAQBAJ&rdid=book-qKFDDAAAQBAJ&rdot=1&source=gbs_api\",\n" +
-            "    \"offers\": [\n" +
-            "     {\n" +
-            "      \"finskyOfferType\": 1,\n" +
-            "      \"listPrice\": {\n" +
-            "       \"amountInMicros\": 2.1875E11,\n" +
-            "       \"currencyCode\": \"IDR\"\n" +
-            "      },\n" +
-            "      \"retailPrice\": {\n" +
-            "       \"amountInMicros\": 1.4875E11,\n" +
-            "       \"currencyCode\": \"IDR\"\n" +
-            "      }\n" +
-            "     }\n" +
-            "    ]\n" +
-            "   },\n" +
-            "   \"accessInfo\": {\n" +
-            "    \"country\": \"ID\",\n" +
-            "    \"viewability\": \"PARTIAL\",\n" +
-            "    \"embeddable\": true,\n" +
-            "    \"publicDomain\": false,\n" +
-            "    \"textToSpeechPermission\": \"ALLOWED\",\n" +
-            "    \"epub\": {\n" +
-            "     \"isAvailable\": false\n" +
-            "    },\n" +
-            "    \"pdf\": {\n" +
-            "     \"isAvailable\": true,\n" +
-            "     \"acsTokenLink\": \"http://books.google.co.id/books/download/Android-sample-pdf.acsm?id=qKFDDAAAQBAJ&format=pdf&output=acs4_fulfillment_token&dl_type=sample&source=gbs_api\"\n" +
-            "    },\n" +
-            "    \"webReaderLink\": \"http://play.google.com/books/reader?id=qKFDDAAAQBAJ&hl=&printsec=frontcover&source=gbs_api\",\n" +
-            "    \"accessViewStatus\": \"SAMPLE\",\n" +
-            "    \"quoteSharingAllowed\": false\n" +
-            "   },\n" +
-            "   \"searchInfo\": {\n" +
-            "    \"textSnippet\": \"Many universities in India and across the world are now teaching Android in their syllabus, which shows the importance of this subject. This book can be read by anyone who knows Java and XML concepts.\"\n" +
-            "   }\n" +
-            "  }\n" +
-            " ]\n" +
-            "}";
+    private static final String BOOKS_API_REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=android";
 
     private QueryUtils(){
 
     }
 
-    public static ArrayList<Book> extractBooks(){
+    /**
+     * Fetch book data and return an {@link List<Book>} object to represent
+     * a list of books
+     */
+    public static List<Book> fetchBooksData(String requestUrl){
+
+        URL url = createUrl(requestUrl);
+
+        String jsonResponse = null;
+        try{
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e){
+            Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+        }
+
+        // Extract relevant fields from the JSON response and
+        // create an {@link List<Book>} object
+        List<Book> books = extractBooks(jsonResponse);
+
+        Log.e(LOG_TAG, "Fetching the data from List<Book> object");
+
+        // Return the {@link List<Book>} object as the
+        // result fo the {@link BookAsyncTask (to do later)}
+        return books;
+    }
+
+
+    public static ArrayList<Book> extractBooks(String booksJSON){
+
+        // If the JSON string is empty or null, then return early
+        if(TextUtils.isEmpty(booksJSON)){
+            return null;
+        }
 
         // Create an empty ArrayList that we can start adding books to
         ArrayList<Book> books = new ArrayList<>();
@@ -132,7 +71,7 @@ public final class QueryUtils {
         // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try{
             // Create a JSONObject from the SAMPLE_JSON_RESPONSE string
-            JSONObject baseJsonResponse = new JSONObject(SAMPLE_JSON_RESPONSE);
+            JSONObject baseJsonResponse = new JSONObject(BOOKS_API_REQUEST_URL);
 
             // Extract the JSONArray associated with the key called "items",
             // which represents a list of items (or books).
@@ -188,5 +127,81 @@ public final class QueryUtils {
         }
 
         return books;
+    }
+
+    /**
+     *
+     */
+    private static URL createUrl(String stringUrl){
+        URL url = null;
+        try{
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e){
+            Log.e(LOG_TAG, "Error with creating URL", e);
+        }
+        return url;
+    }
+
+    /**
+     *
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+        // if URL is null, we return empty jsonResponse
+        if(url == null){
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if(urlConnection.getResponseCode() == 200){
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error Response code: " +
+                        urlConnection.getResponseCode());
+            }
+        } catch (IOException e){
+            Log.e(LOG_TAG,
+                    "Problem retrieving the book JSON results.", e);
+        } finally {
+            if (urlConnection != null){
+                urlConnection.disconnect();
+            }
+            if (inputStream != null){
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the
+     * whole JSON response from the server.
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if(inputStream != null){
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null){
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 }
