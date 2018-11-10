c32a096e9959581c72ae1ecc66e2493cc1494729
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2018-11-10 10:09:53 +0700

    Implement a message that represents no internet connection state

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Parcelable;
@@ -57,11 +59,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
         // Search for SearchView
         searchTextView = (SearchView) findViewById(R.id.search_text);
-
-        // Create a LoaderManager object by calling getLoaderManager() method
-        LoaderManager loaderManager = getLoaderManager();
-
-        loaderManager.initLoader(BOOK_LOADER_ID, null, this);
 
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
         booksListView.setEmptyView(mEmptyStateTextView);
@@ -138,6 +135,29 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
             }
         });
 
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            // Create a LoaderManager object by calling getLoaderManager() method,
+            // which is to interact with loader
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(BOOK_LOADER_ID, null, this);
+        } else {
+            // Hide loading indicator in order for error message to be visible
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet);
+        }
+
     }
 
     @Override
@@ -156,8 +176,19 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
         booksListView.setVisibility(View.VISIBLE);
 
-        // Set empty state text to display "There are no books to display."
-        mEmptyStateTextView.setText(R.string.no_books_data);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            // Set empty state text to display "There are no books to display."
+            mEmptyStateTextView.setText(R.string.no_books_data);
+        } else {
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet);
+        }
 
         // Clear the adapter of previous book data (in order to avoid deletion on empty adapter)
         if(adapter != null){
